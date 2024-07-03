792437be3b6ad1d236a1555c17ec5760d16b6035
introvert730180 <107503675+introvert730180@users.noreply.github.com>
2024-07-03 12:21:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import cv2
 import numpy as np
+import os
 
 # Load pre-trained models
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
@@ -14,21 +15,34 @@ def initialize_caffe_models():
     return age_net, gender_net
 
 # Function to predict age and gender from an image
-def predict_age_gender(image_path, age_net, gender_net):
-    image = cv2.imread(image_path)
-    blob = cv2.dnn.blobFromImage(image, 1, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+def predict_age_gender(image, age_net, gender_net):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
 
-    # Predict Gender
-    gender_net.setInput(blob)
-    gender_preds = gender_net.forward()
-    gender = gender_list[gender_preds[0].argmax()]
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0), 2)
 
-    # Predict Age
-    age_net.setInput(blob)
-    age_preds = age_net.forward()
-    age = age_list[age_preds[0].argmax()]
+        # Get Face 
+        face_img = image[y:y+h, x:x+w].copy()
+        blob = cv2.dnn.blobFromImage(face_img, 1, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
 
-    return gender, age
+        # Predict Gender
+        gender_net.setInput(blob)
+        gender_preds = gender_net.forward()
+        gender = gender_list[gender_preds[0].argmax()]
+
+        # Predict Age
+        age_net.setInput(blob)
+        age_preds = age_net.forward()
+        age = age_list[age_preds[0].argmax()]
+
+        # Display prediction
+        overlay_text = f'Gender: {gender}, Age: {age}'
+        cv2.putText(image, overlay_text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+    return image
+
 
 def main():
     st.title('Age and Gender Prediction using Caffe Models')
@@ -36,7 +50,7 @@ def main():
     # Sidebar file upload widget
     st.sidebar.title('Upload Image')
     uploaded_file = st.sidebar.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-
+    stframe = st.empty()
     if uploaded_file is not None:
         # Display the uploaded image
         image = np.array(bytearray(uploaded_file.read()), dtype=np.uint8)
@@ -53,10 +67,11 @@ def main():
                 f.write(uploaded_file.getbuffer())
 
             # Predict age and gender
-            gender, age = predict_age_gender(image_path, age_net, gender_net)
+            frame = predict_age_gender(image, age_net, gender_net)
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            stframe.image(frame_rgb, channels='RGB', use_column_width=True)
 
             # Display prediction
-            st.success(f'Predicted Gender: {gender}, Predicted Age Range: {age}')
 
             # Remove temporary image file
             os.remove(image_path)
